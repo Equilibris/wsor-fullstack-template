8862fceb9d60b87e74983e93a1e8b699f92f28e4
Equilibris <williambilly42@gmail.com>
2021-12-30 13:50:56 +0100

    V0.5 Add theme-provider

---
+++ libs/context/src/lib/Theme/Theme.tsx
@@ -0,0 +1,58 @@
+import { ThemeProvider as MuiTheme, Theme, useMediaQuery } from '@mui/material'
+import {
+	css,
+	Global,
+	ThemeProvider as EmotionThemeProvider,
+} from '@emotion/react'
+import { FC, useMemo } from 'react'
+
+import { createTheme, Components } from '@mui/material/styles'
+
+const components: Components = {
+	MuiUseMediaQuery: {
+		defaultProps: {
+			noSsr: true,
+		},
+	},
+}
+
+export type ThemeName = 'dark' | 'light'
+
+const themes: Record<ThemeName, Theme> = {
+	light: createTheme({
+		palette: {},
+		components,
+	}),
+	dark: createTheme({
+		palette: {},
+		components,
+	}),
+}
+
+export const ThemeProvider: FC = ({ children }) => {
+	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', {
+		noSsr: true,
+	})
+
+	const theme = useMemo(
+		() => (prefersDarkMode ? themes.dark : themes.light),
+		[prefersDarkMode]
+	)
+
+	return (
+		<MuiTheme theme={theme}>
+			<EmotionThemeProvider theme={theme}>
+				<Global
+					styles={(theme) => css`
+						body {
+							background-color: ${theme.palette.background
+								.default};
+							overflow-x: hidden;
+						}
+					`}
+				/>
+				{children}
+			</EmotionThemeProvider>
+		</MuiTheme>
+	)
+}
